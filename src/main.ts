c43095a5344a2b07a6a56d6ec24fa85cbad275e1
Josh Tomlinson <jtomlinson@cpmstar.com>
2023-10-11 15:53:06 -0700

    Cleaned for repository.

--- src/main.ts
+++
@@ -1,8 +0,0 @@
-import test from "@ogginger/testsuite";
-import * as crawler from "./crawler";
-async function main() {
-    await test( crawler.tests );
-}
-main().catch((err) => {
-  console.error(err);
-});
